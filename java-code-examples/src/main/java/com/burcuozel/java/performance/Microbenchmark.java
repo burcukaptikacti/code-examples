e3aa5205ab27c78f4fd277bf77c57ac413c6bc06
burcukaptikacti <burcukaptikacti@gmail.com>
2021-03-11 13:59:52 +0300

    feature: test not include unneccessary operations

--- java-code-examples/src/main/java/com/burcuozel/java/performance/Microbenchmark.java
+++ java-code-examples/src/main/java/com/burcuozel/java/performance/Microbenchmark.java
@@ -1,5 +1,16 @@
 package com.burcuozel.java.performance;
 
+import java.util.Random;
+
+
+/**
+ * . A microbenchmark is a test designed to measure a very small unit of performance: 
+ * the time to call a synchronized method versus a nonsynchronized method; 
+ * the overhead in creating a thread versus using a thread pool; 
+ * the time to execute one arithmetic algorithm versus an alternate implementation; 
+ *
+ */
+
 public class Microbenchmark {
 
 /**
@@ -27,4 +38,47 @@ private double fibImpl1(int n) {
     double d = fibImpl1(n - 2) + fibImpl1(n  - 1);
     if (Double.isInfinite(d)) throw new ArithmeticException("Overflow");
     return d;
-}}
+}
+
+/**
+ * In this example the time to calculate the random numbers is included that we measure
+ * 
+ * Solution: First generate random numbers array then use it in the loop
+ * 
+ */
+public void doTestfibImpl1_notIncludeUnnecessayOperations() {
+    // Main Loop
+    double l;
+    Random random=new Random();
+    long then = System.currentTimeMillis();
+    for (int i = 0; i < 50; i++) {       
+		l = fibImpl1(random.nextInt());
+    }
+    long now = System.currentTimeMillis();
+    System.out.println("Elapsed time: " + (now - then));
+}
+
+public void doTestfibImpl1_notIncludeUnnecessayOperationsSolution() {
+ 
+    double l;
+    Random random=new Random();
+    int[] input = new int[50];
+    for (int i = 0; i < 50; i++) {
+        input[i] = random.nextInt();
+    }
+    long then = System.currentTimeMillis();
+    for (int i = 0; i < 50; i++) {
+        try {
+            l = fibImpl1(input[i]);
+        } catch (IllegalArgumentException iae) {
+        }
+    }
+    long now = System.currentTimeMillis();
+}
+
+
+
+
+
+
+}
