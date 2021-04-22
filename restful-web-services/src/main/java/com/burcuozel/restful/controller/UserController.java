e90b797acc1530e22e28eb40552a43c054b38d2f
burcukaptikacti <burcukaptikacti@gmail.com>
2021-04-22 21:56:11 +0300

    feature: hateoas,user services, exception handling

--- restful-web-services/src/main/java/com/burcuozel/restful/controller/UserController.java
+++ restful-web-services/src/main/java/com/burcuozel/restful/controller/UserController.java
@@ -1,14 +1,71 @@
 package com.burcuozel.restful.controller;
 
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import java.net.URI;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.burcuozel.restful.exception.UserNotFoundException;
+import com.burcuozel.restful.model.User;
+import com.burcuozel.restful.service.UserService;
 
 @RestController
 public class UserController {
 
-	@GetMapping("/hello-world")
-	public String helloworld() {
-		return "hello World";
+	@Autowired
+	private UserService service;
+
+	@GetMapping("/users")
+	public List<User> retrieveAll() {
+		return service.findAll();
 	}
 
+	@GetMapping("/users/{id}")
+	public EntityModel<User> retrieve(@PathVariable int id) {
+		User user = service.findById(id);
+		if (user == null) {
+			throw new UserNotFoundException("id: " + id);
+		}
+
+		EntityModel<User> resource = EntityModel.of(user);
+
+		WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAll());
+
+		resource.add(linkTo.withRel("all-users"));
+
+		return resource;
+	}
+
+	@PostMapping("/users")
+	public ResponseEntity<Object> create(@Valid @RequestBody User user) {
+		User createdUser = service.save(user);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(createdUser.getId()).toUri();
+
+		return ResponseEntity.created(location).build();
+	}
+
+	@DeleteMapping("/users/{id}")
+	public void deleteUSer(@PathVariable int id) {
+		User user = service.deleteById(id);
+		if (user == null) {
+			throw new UserNotFoundException("id: " + id);
+		}
+	}
 }
