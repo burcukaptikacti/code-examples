9db97be7fae0a77c85681176f5f41c33c6bd1286
burcukaptikacti <burcukaptikacti@gmail.com>
2021-04-30 21:25:56 +0300

    feature: swagger part 2

--- restful-web-services/src/main/java/com/burcuozel/restful/config/SwaggerConfig.java
+++ restful-web-services/src/main/java/com/burcuozel/restful/config/SwaggerConfig.java
@@ -1,8 +1,21 @@
 package com.burcuozel.restful.config;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.hateoas.client.LinkDiscoverer;
+import org.springframework.hateoas.client.LinkDiscoverers;
+import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
+import org.springframework.plugin.core.SimplePluginRegistry;
 
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -11,9 +24,26 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
+	private static final Contact DEFAULT_CONTACT = new Contact("Burcu Özel", "url", "burcuözel@blabla.com");
+	private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<String>(
+			Arrays.asList("application/json", "application/xml"));
+
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2);
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).produces(DEFAULT_PRODUCES_AND_CONSUMES);
 	}
 
+	@Bean
+	public LinkDiscoverers discoverers() {
+		List<LinkDiscoverer> plugins = new ArrayList<>();
+		plugins.add(new CollectionJsonLinkDiscoverer());
+		return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
+
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("API Documentation").description("API Documentation").contact(DEFAULT_CONTACT)
+				.version("1.0.0").build();
+
+	}
 }
